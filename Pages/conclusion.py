d5ca6b0b02b58c24c35471386551a07a271e0120
aliensgtc <94360946+aliensgtc@users.noreply.github.com>
2022-01-16 22:45:15 +0800

    Update conclusion.py

--- Pages/conclusion.py
+++ Pages/conclusion.py
@@ -2,4 +2,17 @@ import streamlit as st
 
 @st.cache
 def conclusion():
+    st.title("Conclusions and Recommendations")
+
+    st.write("")
+    st.subheader("Conclusion")
+    st.write("")
+    conclusion = Image.open("Assets\Images\Conclusion.PNG")
+    st.image(conclusion)
+    
+    st.write("")
+    st.subheader("Recommendations")
+    recommendations = Image.open("Assets\Images\Recommendations.PNG")
+    st.image(recommendations)
+    
     pass
