97ed35c893366a4d8bf9599227236ec11060dd43
Paolo Dano <paolodano1@gmail.com>
2022-01-16 19:56:51 +0800

    added pages functions

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import streamlit as st
 
-from Pages import *
+import pages
+
+def initialize():
+    PAGES = [
+        'Introduction',
+        'Objective',
+        'Time Series Analysis and Forecasting',
+        'Recommendation Engine',
+        'Conclusion and Recommendations'
+    ]
+
+    page = st.sidebar.radio("Page Navigation", PAGES)
+
+    if page == 'Introduction':
+        pages.introduction.introduction()
+    elif page == 'Objective':
+        pages.objective.objective()
+    elif page == 'Time Series Analysis and Forecasting':
+        pages.time_series.time_series()
+    elif page == 'Recommendation Engine':
+        pages.recommendation_engine.recommendation_engine()
+    elif page == 'Conclusion and Recommendations':
+        pages.conclusion.conclusion()
 
 if __name__ == "__main__":
-    pass
+    initialize() 
