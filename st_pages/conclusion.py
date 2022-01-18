ca515122cbb32b933505f1f576db503cefe6178d
Paolo Dano <paolodano1@gmail.com>
2022-01-18 21:51:39 +0800

    Fixed Links to images

--- st_pages/conclusion.py
+++ st_pages/conclusion.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 
+import os
 
 def display():
     st.title("Conclusions and Recommendations")
@@ -8,10 +9,10 @@ def display():
     st.write("")
     st.subheader("Conclusion")
     st.write("")
-    conclusion = Image.open("Assets\Images\Conclusion.png")
+    conclusion = Image.open(os.getcwd() + "\\assets\\images\\conclusion.png")
     st.image(conclusion)
     
     st.write("")
     st.subheader("Recommendations")
-    recommendations = Image.open("Assets\Images\Recommendations.png")
+    recommendations = Image.open(os.getcwd() + "\\assets\\images\\recommendations.png")
     st.image(recommendations)
--- st_pages/objective.py
+++ st_pages/objective.py
@@ -1,19 +1,21 @@
 import streamlit as st
 from PIL import Image
 
+import os
+
 def display():
     st.write("")
     st.subheader("OBJECTIVES")
     objectives = Image.open("Assets\Images\Objectives.png")
     st.image(objectives)
 
-    top_pinoy_rappers = Image.open("Assets\Images\TopPinoyRappers.png")
+    top_pinoy_rappers = Image.open(os.getcwd() + "\\assets\\images\\TopPinoyRappers.png")
     st.image(top_pinoy_rappers)
     
     st.write("")
     st.subheader("METHODOLOGY")
-    forecasting = Image.open("Assets\Images\Method_Forecasting.png")
+    forecasting = Image.open(os.getcwd() + "\\assets\\images\\Method_Forecasting.png")
     st.image(forecasting)
 
-    reco_engine = Image.open("Assets\Images\Method_RecoEngine.png")
+    reco_engine = Image.open(os.getcwd() + "\\assets\\images\\Method_RecoEngine.png")
     st.image(reco_engine)
