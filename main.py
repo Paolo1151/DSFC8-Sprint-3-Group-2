ffba90286727cf9d3b59b077810d88276abcd0fd
Kui-Dong Riman <kim.riman.03@gmail.com>
2022-01-19 13:01:03 +0800

    fix memory

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ def initialize():
     page = st.sidebar.radio("Page Navigation", PAGES)
 
     if page == 'Introduction':
-        introduction.display()
+        on = introduction.display()
     elif page == 'Methodology':
-        methodology.display()
+        on = methodology.display()
     elif page == 'Time Series Analysis and Forecasting':
-        time_series.display()
+        on = time_series.display()
     elif page == 'Recommendation Engine':
         on = Recommender_Page()
         #recommendation_engine.display()
     elif page == 'Conclusion and Recommendations':
-        conclusion.display()
+        on = conclusion.display()
 
 if __name__ == "__main__":
     initialize() 
